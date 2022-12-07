62725a6f1d04661143dceeea32bc5b120f1ce83f
Mateus Faustino <matheusfaustinoe20@gmail.com>
2022-12-07 09:14:58 -0300

    solving a repositivity problem

--- script.js
+++ script.js
@@ -1,31 +1,41 @@
-function clickadd() {
-    const movieName = document.querySelector(".movie-name").value
-    const movieURL = document.querySelector(".movie-url").value
+const form = document.querySelector("form")
 
+form.onsubmit = (event) => {
+  event.preventDefault()
 
-    const img = document.createElement("img")
+  const movieName = event.target[0].value
+  const movieURL = event.target[1].value
 
-    img.src = movieURL
-    img.alt = movieURL
-    img.classList.add('img-banner')
+  addMovie(movieName, movieURL)
 
-    const button = document.createElement("button")
+  form.reset()
+  const inputName = event.target[0]
+  inputName.focus()
+}
 
-    button.classList.add('btn-banner')
-    button.textContent = 'Assistir'
+function addMovie(movieName, url) {
+  const img = document.createElement("img")
+  img.src = url
+  img.alt = url
+  img.classList.add("img-banner")
 
-    const figcaption = document.createElement("figcaption")
+  const button = document.createElement("button")
 
-    figcaption.classList.add('figcap')
-    figcaption.textContent = movieName
+  button.classList.add("btn-banner")
+  button.textContent = "Assistir"
 
-    const figure = document.createElement("figure") 
+  const figcaption = document.createElement("figcaption")
 
-    figure.classList.add('fig-banner')
-    figure.insertAdjacentElement("beforeend", img)
-    figure.insertAdjacentElement("beforeend", button)
-    figure.insertAdjacentElement("beforeend", figcaption) 
+  figcaption.classList.add("figcap")
+  figcaption.textContent = movieName
 
-    const mainBanners = document.querySelector('.main-banners')
-    mainBanners.insertAdjacentElement('beforeend', figure)
+  const figure = document.createElement("figure")
+
+  figure.classList.add("fig-banner")
+  figure.insertAdjacentElement("beforeend", img)
+  figure.insertAdjacentElement("beforeend", button)
+  figure.insertAdjacentElement("beforeend", figcaption)
+
+  const mainBanners = document.querySelector(".main-banners")
+  mainBanners.insertAdjacentElement("beforeend", figure)
 }
